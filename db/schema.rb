71f97f7945da5c8a97008eef7cb73aa1a0493944
eri.morita <morieri.m@gmail.com>
2020-03-03 05:55:17 +0000

    add colum

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_03_015451) do
+ActiveRecord::Schema.define(version: 2020_03_03_054122) do
 
   create_table "messages", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "title"
   end
 
 end
